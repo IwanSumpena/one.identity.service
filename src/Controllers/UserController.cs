ba8caaa8c920aaedbc35954acd7643699ef67512
My LocalV310 <sumpenaiwan@gmail.com>
2021-04-14 22:14:52 +0700

    add user add route

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using src.Models.Entities;
+using src.Models.RequestModel;
 using src.Models.ResponseModel;
 using System;
 using System.Collections.Generic;
@@ -39,6 +40,48 @@ namespace src.Controllers
                 Message = "Berhasil mengambil data.",
                 Data = userData
             });
+        }
+
+        [HttpPost("User/Add")]
+        public async Task<ActionResult<OneResponse<UserResponse>>> Add([FromBody] UserRequest userRequest)
+        {
+            if (string.IsNullOrEmpty(userRequest.Email) || string.IsNullOrEmpty(userRequest.Password))
+            {
+                return BadRequest(new OneResponse<UserResponse>()
+                {
+                    Status = "failed",
+                    Message = "Data ada yang kosong.",
+                    Data = new UserResponse()
+                });
+            }
+
+            var userCreate = new UserOne { UserName = userRequest.Email, Email = userRequest.Email };
+            var identityResult = await _userManager.CreateAsync(userCreate, userRequest.Password);
+            if (identityResult.Succeeded)
+            {
+                return Ok(new OneResponse<UserResponse>()
+                {
+                    Status = "SUCCESS",
+                    Message = "Berhasil mengambil data.",
+                    Data = new UserResponse
+                    {
+                        Id = userCreate.Id,
+                        UserName = userCreate.UserName
+                    }
+                });
+            }
+
+            var error = String.Join(" ", identityResult.Errors.Select(p => p.Description).ToArray());
+
+            return BadRequest(new OneResponse<UserResponse>()
+            {
+                Status = "FIELD",
+                Message = error,
+                Data = new UserResponse()
+            });
+
+
+
         }
     }
 }
