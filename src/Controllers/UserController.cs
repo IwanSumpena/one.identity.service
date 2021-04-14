885260bafef993b4fab4806136f3d7ff11851bb1
My LocalV310 <sumpenaiwan@gmail.com>
2021-04-14 22:09:15 +0700

    add userController

---
+++ src/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using src.Models.Entities;
+using src.Models.ResponseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<UserOne> _userManager;
+
+        public UserController(UserManager<UserOne> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet("User/Users")]
+        public async Task<ActionResult<OneResponse<IEnumerable<UserResponse>>>> Users()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var userData = new List<UserResponse>();
+            foreach (var item in users)
+            {
+                var user = new UserResponse
+                {
+                    Id = item.Id,
+                    UserName = item.UserName
+                };
+                userData.Add(user);
+            }
+            return Ok(new OneResponse<IEnumerable<UserResponse>>()
+            {
+                Status = "SUCCESS",
+                Message = "Berhasil mengambil data.",
+                Data = userData
+            });
+        }
+    }
+}
---
+++ src/Models/ResponseModel/OneResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace src.Models.ResponseModel
+{
+    public class OneResponse<TValue>
+    {
+        [JsonProperty(PropertyName = "status")]
+        public string Status { get; set; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+
+        [JsonProperty(PropertyName = "data")]
+        public TValue Data { get; set; }
+    }
+}
