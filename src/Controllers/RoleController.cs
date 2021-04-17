56e825fb2b5995f233d68456796305d7fe343c88
My LocalV310 <sumpenaiwan@gmail.com>
2021-04-17 19:06:45 +0700

    Add RoleController

---
+++ src/Controllers/RoleController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using src.Models.Entities;
+using src.Models.ResponseModel;
+using src.Utilities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace src.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<RoleOne> _roleManager;
+
+        public RoleController(RoleManager<RoleOne> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet("Role")]
+        public async Task<ActionResult<OneResponse<IEnumerable<RoleResponse>>>> Roles()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            var roleData = new List<RoleResponse>();
+            foreach (var item in roles)
+            {
+                var role = new RoleResponse
+                {
+                    Id = item.Id,
+                    RoleName = item.Name,
+                    Description=""
+                };
+                roleData.Add(role);
+            }
+            return Ok(new OneResponse<IEnumerable<RoleResponse>>()
+            {
+                Status = AppConstans.Response_Status_Success,
+                Message = "Berhasil mengambil data.",
+                Data = roleData
+            });
+        }
+    }
+}
---
+++ src/Models/ResponseModel/RoleResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace src.Models.ResponseModel
+{
+    public class RoleResponse
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        public string Description { get; set; }
+    }
+}
